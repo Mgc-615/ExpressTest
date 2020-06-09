0e7bc981d413e6d8cafb5c7d3d6dd5b978ccebf4
Mgc-615 <712696251@qq.com>
2020-06-09 17:02:41 +0800

    简单增删改查实现

---
+++ db.js
@@ -0,0 +1,31 @@
+/*
+ * @Author: your name
+ * @Date: 2020-06-08 13:18:38
+ * @LastEditTime: 2020-06-08 17:02:08
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \express\myTest\db.js
+ */ 
+var mongoose = require("mongoose"); //引入mongoose
+mongoose.connect('mongodb://localhost/testdb'); //连接到mongoDB的todo数据库
+//该地址格式：mongodb://[username:password@]host:port/database[?options]
+//默认port为27017 
+ 
+var db = mongoose.connection;
+db.on('error', function callback() { //监听是否有异常
+    console.log("Connection error");
+});
+db.once('open', function callback() { //监听一次打开
+    //在这里创建你的模式和模型
+    console.log('connected!');
+});
+ 
+var ListSchema = new mongoose.Schema({
+    user_id: String, //定义一个属性user_id，类型为String
+    content: String, //定义一个属性content，类型为String
+    updated_at: Date //定义一个属性updated_at，类型为Date
+});
+ 
+mongoose.model('userList', ListSchema); //将该Schema发布为Model,user就是集合名称
+
+module.exports = {User:mongoose.model('userList', ListSchema)};
